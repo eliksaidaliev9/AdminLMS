6bcc658e6c646ec013bbf072c9509c7bca1a1ff5
Elyor <elik.saidaliev9@gmail.com>
2025-10-09 01:25:17 +0500

    add admin login page

--- adminapp/test_encoding.py
+++
@@ -1,2 +0,0 @@
-with open('config/settings.py', encoding='utf-8') as f:
-    f.read()
---
+++ config/test_encoding.py
@@ -0,0 +1,3 @@
+with open('config/settings.py', encoding='utf-8') as f:
+    f.read()
+    print("✅ Fayl UTF-8 formatda. Xatolik yo'q.")

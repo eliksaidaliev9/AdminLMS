255f5b49f18305f42a59bcadcbe7df27765fd7d4
Elyor Mahamadjanov <elik.saidaliev9@gmail.com>
2026-01-10 21:06:35 +0500

    Add 'django.contrib.humanize' to installed apps

--- config/settings.py
+++ config/settings.py
@@ -39,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+
     'adminapp',
-    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
